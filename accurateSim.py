628bcf0fcf4100b88488f628815d715cec3e3d77
lrfosso <lrfosso@gmail.com>
2023-02-17 16:24:27 +0100

    bugs

--- accurateSim.py
+++ accurateSim.py
@@ -76,7 +76,18 @@ p_dot = model.set_variable('_z', 'p_dot')
 q_dot = model.set_variable('_z', 'q_dot')
 r_dot = model.set_variable('_z', 'r_dot')
 
-u_vec = model.set_variable('_u', 'uvec', shape=(8,1))
+#u_vec = model.set_variable('_u', 'u_vec', shape=(8,1))
+u_1 = model.set_variable('_u', 'u_1')
+u_2 = model.set_variable('_u', 'u_2')
+u_3 = model.set_variable('_u', 'u_3')
+u_4 = model.set_variable('_u', 'u_4')
+u_5 = model.set_variable('_u', 'u_5')
+u_6 = model.set_variable('_u', 'u_6')
+u_7 = model.set_variable('_u', 'u_7')
+u_8 = model.set_variable('_u', 'u_8')
+u_vec = np.transpose(np.array([u_1,u_2,u_3,u_4,u_5,u_6,u_7,u_8],dtype=object))
+
+
 
 T_mat = np.array([[0.707,0.707,-0.707,-0.707, 0, 0, 0, 0],
 		    [-0.707,0.707,-0.707,0.707, 0, 0, 0, 0 ],
@@ -114,12 +125,12 @@ model.set_rhs('w', w_dot)
 
 #Building the equations of motion for the ROV
 
-M_rb = SX([[m, 0, 0, 0, m*z_g, 0],
+M_rb = np.array([[m, 0, 0, 0, m*z_g, 0],
 		 [0, m, 0, -m*z_g, 0 ,0],
 		 [0, 0, m, 0, 0, 0],
 		 [0, -m*z_g, 0, I_x, 0 ,0],
 		 [m*z_g, 0, 0, 0, I_y, 0],
-		 [0, 0, 0, 0, 0, I_z]])
+		 [0, 0, 0, 0, 0, I_z]],dtype=object)
 print(np.size(M_rb))
 print(v.shape)
 M_a = -np.diag([X_udot,Y_vdot,Z_wdot,K_pdot,M_qdot,N_rdot])
@@ -144,7 +155,7 @@ C_rb = np.array([[0, 0, 0, 0, m*w, 0],
 		 [m*v, -m*u, 0, I_y*q, -I_x*p, 0]], dtype=object)
 
 
-print(model.x)
+print(model.u)
 C = C_a + C_rb
 
 
@@ -161,38 +172,41 @@ D = -np.transpose(np.array([X_u+X_u_abs*u, Y_v+Y_v_abs*v, Z_w+Z_w_abs*v, K_p+K_p
 v_dot_vec = np.transpose(np.array([u_dot,v_dot,w_dot,p_dot,q_dot,r_dot]))
 
 v_vec = np.transpose(np.array([u,v,w,p,q,r],dtype=object))
-print(tau_vec[0].size)
-#dynamics = vertcat(
-#	M[0]@v_dot_vec + C[0]@v_vec + D[0]*v_vec[0] - tau_vec[0],
-#	M[1]@v_dot_vec + C[1]@v_vec + D[1]*v_vec[1] - tau_vec[1],
-#	M[2]@v_dot_vec + C[2]@v_vec + D[2]*v_vec[2] - tau_vec[2],
-#	M[3]@v_dot_vec + C[3]@v_vec + D[3]*v_vec[3] - tau_vec[3],
-#	M[4]@v_dot_vec + C[4]@v_vec + D[4]*v_vec[4] - tau_vec[4],
-#	M[5]@v_dot_vec + C[5]@v_vec + D[5]*v_vec[5] - tau_vec[5])
+
+dynamics = vertcat(
+    M[0]@v_dot_vec + C[0]@v_vec + D[0]@v_vec[0] - tau_vec[0],
+	M[1]@v_dot_vec + C[1]@v_vec + D[1]@v_vec[1] - tau_vec[1],
+	M[2]@v_dot_vec + C[2]@v_vec + D[2]@v_vec[2] - tau_vec[2],
+	M[3]@v_dot_vec + C[3]@v_vec + D[3]@v_vec[3] - tau_vec[3],
+	M[4]@v_dot_vec + C[4]@v_vec + D[4]@v_vec[4] - tau_vec[4],
+	M[5]@v_dot_vec + C[5]@v_vec + D[5]@v_vec[5] - tau_vec[5])
 
 #DAE written out
 #	M				C		D			tau
-f_1 = (m-X_udot)*u_dot + m*z_g*q_dot + (Z_wdot+m)*w*q - (X_u+X_u_abs*fabs(u))*u - (0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2]-0.707*u_vec[3])
-#	M				C			   D				tau
-f_2  = (m-Y_vdot)*v_dot -m*z_g*p_dot - (Z_wdot+m)*w*p - X_udot*u - (Y_v + Y_v_abs*fabs(v))*v - (-0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2] + 0.707*u_vec[3])
-#	M		   C					     D				tau
-f_3 = (m-Z_wdot)*w_dot + (m*v*p - m*u*q -Y_vdot*v*p + X_udot*u*r) - (Z_w+Z_w_abs*fabs(v))*w - ( -u_vec[4] + u_vec[5] + u_vec[6] - u_vec[7])
-#	M				   C
-f_4 = (-m*z_g*v_dot + (I_x-K_pdot)*p_dot) + (-Z_wdot*w+m*w)*v + (-m*v+Y_vdot*v)*w + (I_z*r-N_rdot*r)*q + (-I_y*q+M_qdot*q)*r + (K_p+K_p_abs*fabs(p))*p - (0.06*u_vec[0] - 0.06*u_vec[1] + 0.06*u_vec[2] - 0.06*u_vec[3] -0.218*u_vec[4] - 0.218*u_vec[5] + 0.218*u_vec[6] + 0.218*u_vec[7])
+#f_1 = (m-X_udot)*u_dot + m*z_g*q_dot + (Z_wdot+m)*w*q - (X_u+X_u_abs*fabs(u))*u - (0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2]-0.707*u_vec[3])
+##	M				C			   D				tau
+#f_2  = (m-Y_vdot)*v_dot -m*z_g*p_dot - (Z_wdot+m)*w*p - X_udot*u - (Y_v + Y_v_abs*fabs(v))*v - (-0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2] + 0.707*u_vec[3])
+##	M		                C					     D				tau
+#f_3 = (m-Z_wdot)*w_dot + (m*v*p - m*u*q -Y_vdot*v*p + X_udot*u*r) - (Z_w+Z_w_abs*fabs(v))*w - ( -u_vec[4] + u_vec[5] + u_vec[6] - u_vec[7])
+##	M				                        C
+#f_4 = (-m*z_g*v_dot + (I_x-K_pdot)*p_dot) + (-Z_wdot*w+m*w)*v + (-m*v+Y_vdot*v)*w + (I_z*r-N_rdot*r)*q + (-I_y*q+M_qdot*q)*r + (K_p+K_p_abs*fabs(p))*p - (0.06*u_vec[0] - 0.06*u_vec[1] + 0.06*u_vec[2] - 0.06*u_vec[3] -0.218*u_vec[4] - 0.218*u_vec[5] + 0.218*u_vec[6] + 0.218*u_vec[7])
+##       M                           
+#f_5 = (m*z_g*u_dot - M_qdot*q_dot) + (-m*w*u - m*u*w -I_z*r*p + I_x*p*r) + (Z_wdot*w*u - X_udot*u*w + N_rdot*r*p - K_pdot*p*r) - (M_q+M_q_abs*fabs(q))*q - (0.06*u_vec[0] + 0.06*u_vec[1] - 0.06*u_vec[2] - 0.06*u_vec[3] + 0.120*u_vec[4] - 0.120*u_vec[5] + 0.120*u_vec[6] - 0.120*u_vec[7])
+##       M
+#f_6 = (I_z-N_rdot)*r_dot + (m*v*u - m*u*v + I_y*q*p - I_x*p*q) + (-Y_vdot*v*u + X_udot*u*v - M_qdot*q*p + K_pdot*p*q) -(N_r+N_rdot*fabs(r))*r - (-0.1888*u_vec[0] + 0.1888*u_vec[1] + 0.1888*u_vec[2] - 0.1888*u_vec[3])
 
-f_5 =
+#f_1 = (u_dot - u-u_vec[1])
+#f_2 = (v_dot - v-u_vec[1])
+#f_3 = (w_dot - w-u_vec[1])
+#f_4 = (p_dot - p-u_vec[1])
+#f_5 = (q_dot - q-u_vec[1])
+#f_6 = (r_dot - r-u_vec[1])
 
 
 
 
-print("u")
-print(u)
-print("v")
-print(v)
 
-
-
-dynamics = vertcat(f_1,f_2,f_3,f_4)
+#dynamics = vertcat(f_1,f_2,f_3,f_4,f_5,f_6)
 
 model.set_alg('dynamics',dynamics)
 
@@ -200,4 +214,95 @@ model.setup()
 
 
 
+mpc = do_mpc.controller.MPC(model)
 
+setup_mpc = {
+        'n_horizon':20,
+        't_step':0.1,
+        'n_robust':0,
+        'store_full_solution':True,
+
+        }
+
+mpc.set_param(**setup_mpc)
+_x = model.x
+_u = model.u
+mterm = ((_x['x']-2)**2+ (_x['y']-2)**2+(_x['z']-2)**2)
+lterm = ((_x['x']-2)**2+ (_x['y']-2)**2+(_x['z']-2)**2)
+
+mpc.set_rterm(
+        u_1 = 1,
+        u_2 = 1,
+        u_3 = 1,
+        u_4 = 1,
+        u_5 = 1,
+        u_6 = 1,
+        u_7 = 1,
+        u_8 = 1
+        )
+
+
+mpc.set_objective(mterm=mterm,lterm=lterm)
+
+mpc.setup()
+
+estimator = do_mpc.estimator.StateFeedback(model)
+
+
+simulator = do_mpc.simulator.Simulator(model)
+
+
+params_simulator = {
+    # Note: cvode doesn't support DAE systems.
+    'integration_tool': 'idas',
+    'abstol': 0.001,
+    'reltol': 0.001,
+    't_step': 0.1,
+}
+
+simulator.set_param(**params_simulator)
+
+simulator.setup()
+
+#x0 = np.array([20, -11.4, -1.5, 10, 20, 20, -10, 1,1,2,3,4]).reshape(-1,1)
+x0 = np.array([0, 20, 20, 20, 20, 20, 20, 0,0,0,0,0]).reshape(-1,1)
+mpc.x0 = x0
+estimator.x0 = x0
+simulator.x0 = x0
+
+mpc.set_initial_guess()
+
+mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
+sim_graphics = do_mpc.graphics.Graphics(simulator.data)
+
+fig, ax = plt.subplots(2, sharex=True)
+fig.align_ylabels
+
+
+for g in [sim_graphics, mpc_graphics]:
+    # Plot the angle positions (phi_1, phi_2, phi_2) on the first axis:
+    g.add_line(var_type='_x', var_name='x', axis=ax[0])
+    g.add_line(var_type='_x', var_name='y', axis=ax[0])
+    g.add_line(var_type='_x', var_name='z', axis=ax[0])
+    g.add_line(var_type='_x', var_name='u_vec'[0], axis=ax[0])
+
+    # Plot the set motor positions (phi_m_1_set, phi_m_2_set) on the second axis:
+
+
+ax[0].set_ylabel('angle position [rad]')
+
+#u0 = mpc.make_step(x0)
+#y_next = simulator.make_step(u0)
+for i in range(100):
+    u0 = mpc.make_step(x0)
+    y_next = simulator.make_step(u0)
+    x0 = estimator.make_step(y_next)
+
+lines = (sim_graphics.result_lines['_x', 'y'])
+ax[0].legend(lines,'y',title='hm')
+
+sim_graphics.plot_results()
+
+sim_graphics.reset_axes()
+
+plt.show()
