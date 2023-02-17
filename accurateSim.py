a7582fa542f6b0397adb1ce3e9455681ea7bbb59
lrfosso <lrfosso@gmail.com>
2023-02-17 21:39:55 +0100

    12

--- accurateSim.py
+++ accurateSim.py
@@ -173,27 +173,27 @@ v_dot_vec = np.transpose(np.array([u_dot,v_dot,w_dot,p_dot,q_dot,r_dot]))
 
 v_vec = np.transpose(np.array([u,v,w,p,q,r],dtype=object))
 
-dynamics = vertcat(
-    M[0]@v_dot_vec + C[0]@v_vec + D[0]*v_vec[0] - tau_vec[0],
-	M[1]@v_dot_vec + C[1]@v_vec + D[1]*v_vec[1] - tau_vec[1],
-	M[2]@v_dot_vec + C[2]@v_vec + D[2]*v_vec[2] - tau_vec[2],
-	M[3]@v_dot_vec + C[3]@v_vec + D[3]*v_vec[3] - tau_vec[3],
-	M[4]@v_dot_vec + C[4]@v_vec + D[4]*v_vec[4] - tau_vec[4],
-	M[5]@v_dot_vec + C[5]@v_vec + D[5]*v_vec[5] - tau_vec[5])
+#dynamics = vertcat(
+#    M[0]@v_dot_vec + C[0]@v_vec + D[0]*v_vec[0] - tau_vec[0],
+#	M[1]@v_dot_vec + C[1]@v_vec + D[1]*v_vec[1] - tau_vec[1],
+#	M[2]@v_dot_vec + C[2]@v_vec + D[2]*v_vec[2] - tau_vec[2],
+#	M[3]@v_dot_vec + C[3]@v_vec + D[3]*v_vec[3] - tau_vec[3],
+#	M[4]@v_dot_vec + C[4]@v_vec + D[4]*v_vec[4] - tau_vec[4],
+#	M[5]@v_dot_vec + C[5]@v_vec + D[5]*v_vec[5] - tau_vec[5])
 
 #DAE written out
 #	M				C		D			tau
-#f_1 = (m-X_udot)*u_dot + m*z_g*q_dot + (Z_wdot+m)*w*q - (X_u+X_u_abs*fabs(u))*u - (0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2]-0.707*u_vec[3])
-##	M				C			   D				tau
-#f_2  = (m-Y_vdot)*v_dot -m*z_g*p_dot - (Z_wdot+m)*w*p - X_udot*u - (Y_v + Y_v_abs*fabs(v))*v - (-0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2] + 0.707*u_vec[3])
-##	M		                C					     D				tau
-#f_3 = (m-Z_wdot)*w_dot + (m*v*p - m*u*q -Y_vdot*v*p + X_udot*u*r) - (Z_w+Z_w_abs*fabs(v))*w - ( -u_vec[4] + u_vec[5] + u_vec[6] - u_vec[7])
-##	M				                        C
-#f_4 = (-m*z_g*v_dot + (I_x-K_pdot)*p_dot) + (-Z_wdot*w+m*w)*v + (-m*v+Y_vdot*v)*w + (I_z*r-N_rdot*r)*q + (-I_y*q+M_qdot*q)*r + (K_p+K_p_abs*fabs(p))*p - (0.06*u_vec[0] - 0.06*u_vec[1] + 0.06*u_vec[2] - 0.06*u_vec[3] -0.218*u_vec[4] - 0.218*u_vec[5] + 0.218*u_vec[6] + 0.218*u_vec[7])
-##       M                           
-#f_5 = (m*z_g*u_dot - M_qdot*q_dot) + (-m*w*u - m*u*w -I_z*r*p + I_x*p*r) + (Z_wdot*w*u - X_udot*u*w + N_rdot*r*p - K_pdot*p*r) - (M_q+M_q_abs*fabs(q))*q - (0.06*u_vec[0] + 0.06*u_vec[1] - 0.06*u_vec[2] - 0.06*u_vec[3] + 0.120*u_vec[4] - 0.120*u_vec[5] + 0.120*u_vec[6] - 0.120*u_vec[7])
-##       M
-#f_6 = (I_z-N_rdot)*r_dot + (m*v*u - m*u*v + I_y*q*p - I_x*p*q) + (-Y_vdot*v*u + X_udot*u*v - M_qdot*q*p + K_pdot*p*q) -(N_r+N_rdot*fabs(r))*r - (-0.1888*u_vec[0] + 0.1888*u_vec[1] + 0.1888*u_vec[2] - 0.1888*u_vec[3])
+f_1 = (m-X_udot)*u_dot + m*z_g*q_dot + (Z_wdot+m)*w*q - (X_u+X_u_abs*fabs(u))*u - (0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2]-0.707*u_vec[3])
+#	M				C			   D				tau
+f_2  = (m-Y_vdot)*v_dot -m*z_g*p_dot - (Z_wdot+m)*w*p - X_udot*u - (Y_v + Y_v_abs*fabs(v))*v - (-0.707*u_vec[0] + 0.707*u_vec[1]-0.707*u_vec[2] + 0.707*u_vec[3])
+#	M		                C					     D				tau
+f_3 = (m-Z_wdot)*w_dot + (m*v*p - m*u*q -Y_vdot*v*p + X_udot*u*r) - (Z_w+Z_w_abs*fabs(v))*w - ( -u_vec[4] + u_vec[5] + u_vec[6] - u_vec[7])
+#	M				                        C
+f_4 = (-m*z_g*v_dot + (I_x-K_pdot)*p_dot) + (-Z_wdot*w+m*w)*v + (-m*v+Y_vdot*v)*w + (I_z*r-N_rdot*r)*q + (-I_y*q+M_qdot*q)*r + (K_p+K_p_abs*fabs(p))*p - (0.06*u_vec[0] - 0.06*u_vec[1] + 0.06*u_vec[2] - 0.06*u_vec[3] -0.218*u_vec[4] - 0.218*u_vec[5] + 0.218*u_vec[6] + 0.218*u_vec[7])
+#       M                           
+f_5 = (m*z_g*u_dot - M_qdot*q_dot) + (-m*w*u - m*u*w -I_z*r*p + I_x*p*r) + (Z_wdot*w*u - X_udot*u*w + N_rdot*r*p - K_pdot*p*r) - (M_q+M_q_abs*fabs(q))*q - (0.06*u_vec[0] + 0.06*u_vec[1] - 0.06*u_vec[2] - 0.06*u_vec[3] + 0.120*u_vec[4] - 0.120*u_vec[5] + 0.120*u_vec[6] - 0.120*u_vec[7])
+#       M
+f_6 = (I_z-N_rdot)*r_dot + (m*v*u - m*u*v + I_y*q*p - I_x*p*q) + (-Y_vdot*v*u + X_udot*u*v - M_qdot*q*p + K_pdot*p*q) -(N_r+N_rdot*fabs(r))*r - (-0.1888*u_vec[0] + 0.1888*u_vec[1] + 0.1888*u_vec[2] - 0.1888*u_vec[3])
 
 #f_1 = (u_dot - u-u_vec[1])
 #f_2 = (v_dot - v-u_vec[1])
@@ -206,7 +206,7 @@ dynamics = vertcat(
 
 
 
-#dynamics = vertcat(f_1,f_2,f_3,f_4,f_5,f_6)
+dynamics = vertcat(f_1,f_2,f_3,f_4,f_5,f_6)
 
 model.set_alg('dynamics',dynamics)
 
@@ -227,22 +227,48 @@ setup_mpc = {
 mpc.set_param(**setup_mpc)
 _x = model.x
 _u = model.u
-mterm = ((_x['x']-2)**2+ (_x['y']-2)**2+(_x['z']-2)**2)
-lterm = ((_x['x']-2)**2+ (_x['y']-2)**2+(_x['z']-2)**2)
+mterm = ((_x['x']-5)**2+ (_x['y']-5)**2+(_x['z']-5)**2 + (_x['u']**2  + _x['v']**2+ _x['w']**2)*0.01)
+lterm = ((_x['x']-5)**2+ (_x['y']-5)**2+(_x['z']-5)**2 + (_x['u']**2  + _x['v']**2+ _x['w']**2)*0.01
+       + (u_1**2+u_2**2+u_3**2+u_4**2+u_5**2 + u_6**2+u_7**2+u_8**2)*0.01)
+#mterm = _x['x']**2
+#lterm = _x['x']**2
+
 
 mpc.set_rterm(
-        u_1 = 100,
-        u_2 = 100,
-        u_3 = 100,
-        u_4 = 100,
-        u_5 = 100,
-        u_6 = 100,
-        u_7 = 100,
-        u_8 = 100
+        u_1 = 0.001,
+        u_2 = 0.001,
+        u_3 = 0.001,
+        u_4 = 0.001,
+        u_5 = 0.001,
+        u_6 = 0.001,
+        u_7 = 0.001,
+        u_8 = 0.001
         )
 
 
+
 mpc.set_objective(mterm=mterm,lterm=lterm)
+
+#mpc.bounds['lower','_u', 'u_1'] = - 0.1
+#mpc.bounds['lower','_u', 'u_2'] = - 0.1
+#mpc.bounds['lower','_u', 'u_3'] = - 0.1
+#mpc.bounds['lower','_u', 'u_4'] = - 0.1
+#mpc.bounds['lower','_u', 'u_5'] = - 0.1
+#mpc.bounds['lower','_u', 'u_6'] = - 0.1
+#mpc.bounds['lower','_u', 'u_7'] = - 0.1
+#mpc.bounds['lower','_u', 'u_8'] = - 0.1
+#
+#
+#
+#mpc.bounds['upper','_u', 'u_1'] =  0.1
+#mpc.bounds['upper','_u', 'u_2'] =  0.1
+#mpc.bounds['upper','_u', 'u_3'] =  0.1
+#mpc.bounds['upper','_u', 'u_4'] =  0.1
+#mpc.bounds['upper','_u', 'u_5'] =  0.1
+#mpc.bounds['upper','_u', 'u_6'] =  0.1
+#mpc.bounds['upper','_u', 'u_7'] =  0.1
+#mpc.bounds['upper','_u', 'u_8'] =  0.1
+
 
 mpc.setup()
 
@@ -265,7 +291,7 @@ simulator.set_param(**params_simulator)
 simulator.setup()
 
 #x0 = np.array([20, -11.4, -1.5, 10, 20, 20, -10, 1,1,2,3,4]).reshape(-1,1)
-x0 = np.array([0, 20, 20, 20, 20, 20, 20, 0,0,0,0,0]).reshape(-1,1)
+x0 = np.array([-1, 1, 2, 2, 2, 2, 2, 1,1,2,3,4]).reshape(-1,1)
 mpc.x0 = x0
 estimator.x0 = x0
 simulator.x0 = x0
@@ -284,7 +310,13 @@ for g in [sim_graphics, mpc_graphics]:
     g.add_line(var_type='_x', var_name='x', axis=ax[0])
     g.add_line(var_type='_x', var_name='y', axis=ax[0])
     g.add_line(var_type='_x', var_name='z', axis=ax[0])
-    #g.add_line(var_type='_u', var_name='u_1', axis=ax[0])
+    
+    g.add_line(var_type='_u', var_name='u_1', axis=ax[1])
+    g.add_line(var_type='_u', var_name='u_2', axis=ax[1])
+    g.add_line(var_type='_u', var_name='u_3', axis=ax[1])
+    g.add_line(var_type='_u', var_name='u_4', axis=ax[1])
+    g.add_line(var_type='_u', var_name='u_5', axis=ax[1])
+    g.add_line(var_type='_u', var_name='u_6', axis=ax[1])
 
     # Plot the set motor positions (phi_m_1_set, phi_m_2_set) on the second axis:
 
@@ -294,14 +326,16 @@ ax[0].set_ylabel('angle position [rad]')
 #u0 = mpc.make_step(x0)
 #y_next = simulator.make_step(u0)
 u0 = np.zeros((8,1))
-for i in range(400):
+for i in range(300):
     print(i)
-    u0 = mpc.make_step(x0)
+    #u0 = mpc.make_step(x0)
     y_next = simulator.make_step(u0)
     x0 = estimator.make_step(y_next)
 
-lines = (sim_graphics.result_lines['_x', 'y'])
-ax[0].legend(lines,'y',title='hm')
+lines = (sim_graphics.result_lines['_x', 'x']+
+        sim_graphics.result_lines['_x', 'y']+
+        sim_graphics.result_lines['_x', 'z'])
+ax[0].legend(lines,'xyz',title='position')
 
 sim_graphics.plot_results()
 
