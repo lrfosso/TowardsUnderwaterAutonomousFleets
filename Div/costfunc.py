1de5d4c029ee7cb8c137e710e70b3c36fad6a5db
jeghetertor <torstaurnes@msn.com>
2023-03-22 14:57:53 +0100

    Cost func quat

---
+++ Div/costfunc.py
@@ -0,0 +1,7 @@
+            case 4:
+                mterm = (1.8*(_x_rov1['x'] + 4)**2 + 3*(_x_rov1['y'] - 7)**2 +  2*(_x_rov1['z'] - _tvp_rov1['z_sp'])**2 
+                +2*((((_x_rov1['q_0']*_tvp_rov1['q_0_sp']+_x_rov1['q_1']* _tvp_rov1['q_1_sp']+_x_rov1['q_2']* _tvp_rov1['q_2_sp']+_x_rov1['q_3']* _tvp_rov1['q_3_sp'])-1)**2 )
+                +(-_x_rov1['q_1_sp']*_x_rov1['q_0']+_x_rov1['q_0_sp']*_x_rov1['q_1']-_x_rov1['q_3_sp']*_x_rov1['q_2']+_x_rov1['q_2_sp']*_x_rov1['q_3'])**2
+                +(-_x_rov1['q_2_sp']*_x_rov1['q_0']+_x_rov1['q_3_sp']*_x_rov1['q_1']+_x_rov1['q_0_sp']*_x_rov1['q_2']-_x_rov1['q_1_sp']*_x_rov1['q_3'])**2
+                +(-_x_rov1['q_3_sp']*_x_rov1['q_0']-_x_rov1['q_2_sp']*_x_rov1['q_1']+_x_rov1['q_1_sp']*_x_rov1['q_2']+_x_rov1['q_0_sp']*_x_rov1['q_3'])**2))
+                lterm = mterm + (_u_rov1['u_1']**2+_u_rov1['u_2']**2+_u_rov1['u_3']**2+_u_rov1['u_4']**2+_u_rov1['u_5']**2 + _u_rov1['u_6']**2+_u_rov1['u_7']**2+_u_rov1['u_8']**2)*0.03
