56cf870c2cc149c65130b71ca1536dd249240302
Pablo <80708206+PauloNTNU@users.noreply.github.com>
2023-03-31 14:29:59 +0200

    ROS2 Package - MPC-controller
    params.yaml added and can be used to launch multiple nodes based on multi_agent variable in params.yaml & mpc1 is getting parameters from params.yaml

---
+++ GazeboSimulator/mpc_controller/launch/multi_mpc_launch.py
@@ -0,0 +1,59 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+import sys
+import yaml
+
+#Setup of Launch
+def multi_launch(context, *args, **kwargs):
+    #multi_agent = int(LaunchConfiguration('multi_agent').perform(context))
+
+    #Getting the file mpc_controller/params/params.yaml
+    param_path = os.path.join(
+    get_package_share_directory('mpc_controller'),
+    'params',
+    'params.yaml'
+    )
+    with open(param_path, 'r') as f:
+        config = yaml.safe_load(f)
+        param = config['/**']['ros_parameters']
+
+    multi_agent = param['multi_agent']
+
+    #Creating a node for each ROV
+    launch_agents = []
+    for agent_id in range(multi_agent):
+        agent = Node(
+            package='mpc_controller',
+            namespace='bluerov{}_mpc'.format(agent_id+2),
+            name='bluerov{}'.format(agent_id+2),
+            executable='bluerov_mpc',
+            #output='screen',
+            #prefix=['gnome-terminal --command gdb'],
+            parameters= [param,
+            {
+                'main_id': agent_id+2,
+                'fleet_quantity': multi_agent,
+            }
+            ]
+        )
+        launch_agents.append(agent)
+
+    trajectory_node = Node(
+       package='mpc_controller',
+       executable='setpoint',
+       output='screen'
+    )
+    launch_agents.append(trajectory_node)
+
+    return launch_agents
+
+#Launch
+def generate_launch_description():
+    return LaunchDescription([
+        #DeclareLaunchArgument('multi_agent', default_value='1'),
+        OpaqueFunction(function=multi_launch)
+    ])
---
+++ GazeboSimulator/mpc_controller/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'mpc_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rov',
+    maintainer_email='rov@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'bluerov_mpc = mpc_controller.mpc_controller:main',
+        'setpoint = mpc_controller.setpoint_publisher:main'
+        ],
+    },
+)
