2e1c7df192afeb5a99c39230197d1423821ffa28
jeghetertor <torstaurnes@msn.com>
2023-04-23 17:35:30 +0200

    Changes plotting programs
    Made general plotting for files in a folder for 2D and 3D

---
+++ dataresultat/2D_plot_program.py
@@ -0,0 +1,162 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import os
+
+
+#----------------------------Settings----------------------------#
+n_agents = 2
+folder = "current_z03"
+save_fig = True
+display_fig = False
+
+#----------------------------Functions----------------------------#
+def full_sec(df):
+    """Convert from sec and nanosec to full seconds"""
+    full_sec = []
+    init_time = df['sec'][0] + df['nanosec'][0]/1000000000
+    for i, val in enumerate(df['sec']):
+        full_sec.append((val + df['nanosec'][i]/1000000000)-init_time)
+    return full_sec
+
+def xyz_plot(df1, df2, save_fig, display_fig):
+    full_sec1 = full_sec(df1)
+    full_sec2 = full_sec(df2)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+
+    fig.suptitle('Position')
+    ax1.plot(full_sec1, df1['x_ref'], 'r', label='ref_x')
+    ax1.plot(full_sec1, df1['x'], 'b', label='x1')
+    ax1.plot(full_sec2, df2['x'], 'g', label='x2')
+    ax1.legend(loc='upper left')
+    ax1.grid()
+
+    ax2.plot(full_sec1, df1['y_ref'], 'r', label='ref_y')
+    ax2.plot(full_sec1, df1['y'], 'b', label='y1')
+    ax2.plot(full_sec2, df2['x'], 'g', label='y2')
+    ax2.legend(loc='upper left')
+    ax2.grid()
+
+    ax3.plot(full_sec1, df1['z_ref'], 'r', label='ref_z')
+    ax3.plot(full_sec1, df1['z'], 'b', label='z1')
+    ax3.plot(full_sec2, df2['x'], 'g', label='z2')
+    ax3.legend(loc='upper left')
+    ax3.grid()
+
+
+    plot_name = "2D_xyz_"+file[45:].replace(".csv", ".png")
+    plot_name = plot_name.replace("--rov2", "")
+    if(save_fig):
+        print("Saving figure as:",plot_name)
+        plt.savefig((folder+"/"+plot_name), dpi=300)
+    if(display_fig):
+        plt.show()
+
+def angle_plot(df1,df2, save_fig, display_fig):
+    full_sec1 = full_sec(df1)
+    full_sec2 = full_sec(df2)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(full_sec1, df1['angle2'],'b', label='FOV angle 1')
+    ax.plot(full_sec2, df2['angle2'],'g', label='FOV angle 2')
+    ax.plot(full_sec1, [90 for i in range(len(full_sec1))], 'r', label='FOV limit')
+
+    ax.set(xlabel='time (s)', ylabel='angle (deg)',title="FOV angle")
+    ax.grid()
+
+    plot_name = "2D_FOV_"+file[45:].replace(".csv", ".png")
+    plot_name = plot_name.replace("--rov2", "")
+    if(save_fig):
+        print("Saving figure as:",plot_name)
+        plt.savefig((folder+"/"+plot_name), dpi=300)
+    if(display_fig):
+        plt.show()
+
+def distance_rovs(df1,df2, save_fig, display_fig):
+    #### FUNKER IKKE
+    full_sec1 = full_sec(df1)
+    full_sec2 = full_sec(df2)
+
+    fig, ax = plt.subplots()
+    distance = []
+    if(len(full_sec1)<len(full_sec2)):
+        for i in range(len(full_sec1)):
+            distance.append(np.sqrt((df1['x'][i]-df2['x'][i])**2+(df1['y'][i]-df2['y'][i])**2+(df1['z'][i]-df2['z'][i])**2))
+        ax.plot(full_sec1, distance, 'b', label='Distance')
+
+    else:
+        for i in range(len(full_sec2)):
+            distance.append(np.sqrt((df1['x'][i]-df2['x'][i])**2+(df1['y'][i]-df2['y'][i])**2+(df1['z'][i]-df2['z'][i])**2))
+        ax.plot(full_sec2, distance, 'b', label='Distance')
+        
+    ax.set(xlabel='time (s)', ylabel='Distance (m)',title="Distance between ROVs")
+    ax.grid()
+    plot_name = "2D_Dist_ROV_"+file[45:].replace(".csv", ".png")
+    plot_name = plot_name.replace("--rov2", "")
+    if(save_fig):
+        print("Saving figure as:",plot_name)
+        plt.savefig((folder+"/"+plot_name), dpi=300)
+    if(display_fig):
+        plt.show()
+
+def distance_from_ref(df1,df2, save_fig, display_fig):
+    full_sec1 = full_sec(df1)
+    full_sec2 = full_sec(df2)
+
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+    fig.suptitle('Distance from reference')
+
+    ax1.plot(full_sec1, abs(df1['x']-df1['x_ref']), 'b', label='Delta x')
+    ax1.plot(full_sec1, abs(df1['y']-df1['y_ref']), 'g', label='Delta y')
+    ax1.plot(full_sec1, abs(df1['z']-df1['z_ref']), 'r', label='Delta z')
+    ax1.set(ylabel='Distance [m]', title="ROV 1")
+    ax1.legend(loc='upper left')
+    ax1.grid()
+
+    ax2.plot(full_sec2, abs(df2['x']-df2['x_ref']), 'b', label='Delta x')
+    ax2.plot(full_sec2, abs(df2['y']-df2['y_ref']), 'g', label='Delta y')
+    ax2.plot(full_sec2, abs(df2['z']-df2['z_ref']), 'r', label='Delta z')
+    ax2.set(xlabel='time (s)', ylabel='Distance [m]', title="ROV 2")
+    ax2.legend(loc='upper left')
+    ax2.grid()
+
+    plot_name = "2D_Dist_Ref_"+file[45:].replace(".csv", ".png")
+    plot_name = plot_name.replace("--rov2", "")
+    if(save_fig):
+        print("Saving figure as:",plot_name)
+        plt.savefig((folder+"/"+plot_name), dpi=300)
+    if(display_fig):
+        plt.show()
+
+
+#----------------------------Plotting logic----------------------------#   
+files = os.listdir(folder)
+
+
+for i, file in enumerate(files):
+    if (("rov2" in file) and (".csv" in file)):
+        pass
+    else:
+        files[i] = ''
+files = list(filter(None, files))
+    
+
+for file in files:
+    df1 = pd.read_csv(folder+'/'+file, encoding='utf-8', engine='python')
+
+    file2 = file
+    file2 = file2.replace("rov2", "rov3")
+    df2 = pd.read_csv(folder+'/'+file2, encoding='utf-8', engine='python')
+
+    xyz_plot(df1, df2, save_fig, display_fig)
+    angle_plot(df1, df2, save_fig, display_fig)
+    #distance_rovs(df1, df2, save_fig, display_fig)
+    distance_from_ref(df1, df2, save_fig, display_fig)
+
+    
+
+
--- dataresultat/3D_plot_program.py
+++ dataresultat/3D_plot_program.py
@@ -3,16 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
+import os
 
 #----------------------------Settings----------------------------#
-n_agents = 3
-df1_name = '2agents1setpoint/C10_57_32_D21_04_23data2.csv'
-df2_name = '2agents1setpoint/C10_57_32_D21_04_23data3.csv'
-df3_name = '2agents1setpoint/C10_57_32_D21_04_23data4.csv'
-plot_name = '300DPI'
-save_fig = False
-display_fig = True
-radius = 2.0
+n_agents = 2
+folder = "current_x03"
+save_fig = True
+display_fig = False
 
 #----------------------------------------------------------------#
 
@@ -24,109 +21,38 @@ def full_sec(df):
         full_sec.append((val + df['nanosec'][i]/1000000000)-init_time)
     return full_sec
 
+files = os.listdir(folder)
 
-# Read data from csv file
-df1 = pd.read_csv(df1_name)
-df1.columns = ['x_ref','y_ref','z_ref','x','y','z','eta','e1','e2','e3','u','v','w','p','q','r','sec','nanosec','angle2','angle3','control_mode','std_test']
-
-full_sec1 = full_sec(df1)
-if(n_agents > 1):
-    df2 = pd.read_csv(df2_name)
-    df2.columns = ['x_ref','y_ref','z_ref','x','y','z','eta','e1','e2','e3','u','v','w','p','q','r','sec','nanosec','angle2','angle3','control_mode','std_test']
-    full_sec2 = full_sec(df2)
-if(n_agents > 2):
-    df3 = pd.read_csv(df3_name)
-    df3.columns = ['x_ref','y_ref','z_ref','x','y','z','eta','e1','e2','e3','u','v','w','p','q','r','sec','nanosec','angle2','angle3','control_mode','std_test']
-    full_sec3 = full_sec(df3)
-
-
-#print(df1.head())
-#print(df1.sec)
-# Convert from sec and nanosec to full seconds
-
-
-fig = plt.figure()
- 
-# syntax for 3-D projection
-ax = plt.axes(projection ='3d')
-
-def x_torus(t):
-    r = 1
-    r_big = 5
-    return r_big*np.cos(np.pi*(t*0.5)/150)+r*np.cos(np.pi*(t*0.5)/25)*np.cos(np.pi*(t*0.5)/150) -(r_big+r)
-def y_torus(t):
-    r = 1
-    r_big = 3
-    b = 3
-    return r_big*np.sin(np.pi*(t*0.5)/150)+r*np.cos(np.pi*(t*0.5)/25)*np.sin(np.pi*(t*0.5)/150) 
-def z_torus(t):
-    r = 1
-    r_big = 3
-    b = 3
-    return r*np.sin(np.pi*(t*0.5)/25) +5
-
-
-def x_spiral(t):
-    return (4-(t*0.5)*0.15)*np.cos(np.pi*(t*0.5)/(100-0.3*(t*0.5))) - 4
-
-def y_spiral(t):
-    return (4-(t*0.5)*0.15)*np.sin(np.pi*(t*0.5)/(100-0.3*(t*0.5)))
-
-def z_spiral(t):
-    return 5.0
-
-
-def x_line(t):
-    return 1.33226763*10**(-16)*t+1.20000000*10**(-2)*t**2-1.20000000*10**(-4)*t**3
-
-
-
-
-
-
-
-t = int(240*2)
-# plotting
-#ax.plot3D([x_spiral(i) for i in range(t)], [y_spiral(i) for i in range(t)], [z_spiral(i) for i in range(t)], 'gray')
-
-t0 = 0
-avg_speed = 0.3
-t1 = np.sqrt(10 **2)/avg_speed
-dx0 = 0
-dx1 = 0.3
-
-x0 = 0
-x1 = 10
-
-A =np.array([[1,t0, t0**2, t0**3],
-                    [0, 1, 2*t0, 3*t0**2],
-                    [1, t1, t1**2, t1**3],
-                    [0, 1, 2*t1, 3*t1**2]])
+for i, file in enumerate(files):
+    if (("rov2" in file) and (".csv" in file)):
+        pass
+    else:
+        files[i] = ''
+files = list(filter(None, files))
     
-y = np.array([x0,dx0,x1,dx1]).reshape(-1,1)
-b = (np.linalg.inv(A)@y).reshape(1,-1)
-b = b[0]
 
-print(b)
+for file in files:
+    df1 = pd.read_csv(folder+'/'+file)
+    full_sec1 = full_sec(df1)
+    file2 = file
+    file2 = file2.replace("rov2", "rov3")
+    df2 = pd.read_csv(folder+'/'+file2)
+    full_sec2 = full_sec(df2)
 
+    # Read data from csv file
+    fig = plt.figure()
+    
+    # syntax for 3-D projection
+    ax = plt.axes(projection ='3d')
 
-
-
-ax.plot3D(df1['x'], df1['y'], df1['z'], 'gray')
-ax.plot3D(df2['x'], df2['y'], df2['z'], 'black')
-ax.plot3D(df3['x'], df3['y'], df3['z'], 'blue')
-ax.plot3D(df1['x_ref'], df1['y_ref'], df1['z_ref'], 'red')
-
-
-
-
-if(save_fig): 
-    now = datetime.now()
-    dt_string = now.strftime("C%H_%M_%S_D%d_%m_%y")
-    print(type(dt_string))
-    print("Fig saved as: "+'Resulting_figures/'+plot_name+'_'+dt_string+'.png')
-    plt.savefig('Resulting_figures/'+plot_name+'_'+dt_string+'.png', dpi=300)
-if(display_fig):
-    plt.show()
-
+    ax.plot3D(df1['x'], df1['y'], df1['z'], 'gray')
+    ax.plot3D(df2['x'], df2['y'], df2['z'], 'black')
+    ax.plot3D(df1['x_ref'], df1['y_ref'], df1['z_ref'], 'red')
+    plot_name = "3D_"+file[45:].replace(".csv", ".png")
+    plot_name = plot_name.replace("--rov2", "")
+    if(save_fig):
+        print("Saving figure as:",plot_name)
+        plt.savefig((folder+"/"+plot_name), dpi=300)
+    if(display_fig):
+        plt.show()
 
