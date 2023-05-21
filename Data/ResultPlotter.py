ad998330b5c2d91c82ca2d427a174d539060bea0
lrfosso <46139481+lrfosso@users.noreply.github.com>
2023-05-22 00:46:25 +0200

    Update ResultPlotter.py

--- Data/ResultPlotter.py
+++ Data/ResultPlotter.py
@@ -1,7 +1,7 @@
 """
 _______________________________________________________________________________
-This file creates a plot for the comparing the median of the different scenarios
-How to use: 1. place the folders with the csvs in folderList
+This file creates a plot the data from csvs.
+How to use: 1. place the the folder of tests inside a folder, change main_dir to this name.
             2. change test df_"test" to circle, torus line or spiral. on line 270 and 272
 _______________________________________________________________________________
 """
