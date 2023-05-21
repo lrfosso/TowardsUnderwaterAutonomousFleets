089687c3a68898363d3bbcff864537638d978f33
lrfosso <46139481+lrfosso@users.noreply.github.com>
2023-05-22 00:43:02 +0200

    Update median_plotter.py

--- Data/median_plotter.py
+++ Data/median_plotter.py
@@ -1,3 +1,11 @@
+"""
+_______________________________________________________________________________
+This file creates a plot for the comparing the median of the different scenarios
+How to use: 1. place the folders with the csvs in folderList
+            2. change test df_"test" to circle, torus line or spiral.
+_______________________________________________________________________________
+"""
+
 import pandas as pd
 import numpy as np
 import os
